2f85e91560c2a93664e5bfa2678f973fadb82a34
Connor Peebles <connorpeebles93@gmail.com>
2018-09-16 23:29:44 +0000

    Size of chart, spacing size, number of bars, and bar values are now dynamic

--- barchart.js
+++ barchart.js
@@ -1,16 +1,47 @@
-function drawBarChart(data, element) {
-  var chartCon = document.getElementById(element);
-  chartCon.style.width = 300 + "px";
+function drawBarChart(data, options, element) {
+  var chartArea = document.getElementById(element);
+
+  var numBars = data.length;
+  var chartWidth = 500;
+  var chartHeight = 300;
+  var space = 5;
+
+  var maxValue = data[0];
+  for (i = 1; i < data.length; i++) {
+    if (data[i] > maxValue) {
+      maxValue = data[i];
+    }
+  }
+
+  if ("width" in options === true) {
+    chartArea.style.width = options.width + "px";
+    chartWidth = options.width;
+  }
+
+  if ("height" in options === true) {
+    chartArea.style.height = options.height + "px";
+    chartHeight = options.height;
+  }
+
+  if ("spacing" in options === true) {
+    space = options.spacing;
+  }
+
+  var barWidth = (chartWidth / numBars) - space;
+  var barHeight;
 
   for (var i = 0; i < data.length; i++) {
-    var divElement = document.createElement("div");
-    divElement.setAttribute("class", "div2");
+    var bar = document.createElement("div");
+    bar.setAttribute("class", "div2");
 
-    divElement.style.marginLeft = parseInt(i*2 + i*20) + "px";
-    divElement.style.height = parseInt(data[i] * 10) + "px";
-    divElement.style.width = parseInt(i*20) + "px";
-    chartCon.appendChild(divElement);
+    barHeight = data[i] / maxValue * chartHeight;
+
+    bar.style.width = barWidth + "px";
+    bar.style.height = barHeight + "px";
+    bar.style.marginLeft = (space + i * (barWidth + space)) + "px";
+    bar.style.top = (chartHeight - barHeight + 50) + "px";
+    chartArea.appendChild(bar);
   }
 }
 
-drawBarChart([5,20,30], "div1");
+drawBarChart([1,2,4,8,16], {height:200, width:700, spacing:40}, "div1");
