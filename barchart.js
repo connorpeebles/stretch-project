11c9eb399ef2c0e973394b94d19500cd6cca9963
Connor Peebles <connorpeebles93@gmail.com>
2018-09-17 20:27:23 +0000

    Changed the way the X labels are entered

--- barchart.js
+++ barchart.js
@@ -9,10 +9,10 @@ function drawBarChart(data, options, element) {
   var textColour = "#FFFFFF";
   var textAlign = "top";
 
-  var maxValue = data[0];
+  var maxValue = data[0][0];
   for (i = 1; i < data.length; i++) {
-    if (data[i] > maxValue) {
-      maxValue = data[i];
+    if (data[i][0] > maxValue) {
+      maxValue = data[i][0];
     }
   }
 
@@ -49,14 +49,14 @@ function drawBarChart(data, options, element) {
     var bar = document.createElement("div");
     bar.setAttribute("class", "div2");
 
-    barHeight = data[i] / maxValue * chartHeight;
+    barHeight = data[i][0] / maxValue * chartHeight;
 
     bar.style.width = barWidth + "px";
     bar.style.height = barHeight + "px";
     bar.style.marginLeft = (space + i * (barWidth + space)) + "px";
     bar.style.top = (chartHeight - barHeight + 50) + "px";
     bar.style.background = colour;
-    bar.innerHTML = data[i];
+    bar.innerHTML = data[i][0];
     bar.style.color = textColour;
 
     if (textAlign === "center") {
@@ -70,28 +70,34 @@ function drawBarChart(data, options, element) {
     chartArea.appendChild(bar);
   }
 
-  if ("Xlabels" in options === true) {
-    var labels = options.Xlabels;
-    drawXlabels(labels, chartWidth, barWidth, space, "div3");
-  }
+  drawXlabels(data, chartWidth, barWidth, space, "div3");
 
+  drawYlabels(data, chartHeight, "div5");
 }
 
-function drawXlabels(labels, chartWidth, barWidth, space, element) {
+function drawXlabels(data, chartWidth, barWidth, space, element) {
   var labelArea = document.getElementById(element);
   labelArea.style.width = chartWidth + "px";
 
-  for (var i = 0; i < labels.length; i++) {
+  for (var i = 0; i < data.length; i++) {
     var label = document.createElement("div");
     label.setAttribute("class", "div4");
 
     label.style.height = barWidth + "px";
     label.style.marginLeft = (space + i * (barWidth + space)) + "px";
-    label.innerHTML = labels[i];
+    label.innerHTML = data[i][1];
     label.style.lineHeight = barWidth + "px";
 
     labelArea.appendChild(label);
   }
 }
 
-drawBarChart([1,2,4,8,16], {height:500, width:600, spacing:10, colour:"#800080", text:"#C0C0C0", align:"center", Xlabels:["lol","look","at","my","text"]}, "div1");
+
+function drawYlabels(data, chartHeight, element) {
+  var labelArea = document.getElementById(element);
+  labelArea.style.height = chartHeight + "px";
+}
+
+
+
+drawBarChart([[1,"lol"],[2,"look"],[4,"at"],[8,"my"],[16,"text"],[32,"!!!"]], {height:500, width:600, spacing:10, colour:"#800080", text:"#C0C0C0", align:"center"}, "div1");
