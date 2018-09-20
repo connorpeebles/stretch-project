9ed8aa56b47d88c0cafb08cee72e3f4f38d9d7c3
Connor Peebles <connorpeebles93@gmail.com>
2018-09-20 20:58:59 +0000

    Converted the drawLegend function to use jQuery

--- barchart2.js
+++ barchart2.js
@@ -62,7 +62,7 @@ function drawBarChart(data, options, element) {
   } else if (typeof(values[0]) == typeof([])) {
     var legend = data.legend;
     stackedBarChart(values, legend, chartWidth, chartHeight, barWidth, maxY, space, labelColour, labelALign, element);
-    drawLegend(chartWidth, chartHeight, legend, "div8");
+    drawLegend(chartWidth, chartHeight, legend, element);
   }
 
   // calls drawXlabels to draw the labels on the X-axis
@@ -107,7 +107,7 @@ function singleBarChart(values, chartWidth, chartHeight, barWidth, maxY, space, 
   var barHeight;
 
   for (var i = 0; i < values.length; i++) {
-    var bar = $("<div>").addClass("div2");
+    var bar = $("<div>").addClass("bar");
 
     barHeight = values[i] / maxY * chartHeight;
 
@@ -128,16 +128,50 @@ function singleBarChart(values, chartWidth, chartHeight, barWidth, maxY, space, 
   }
 }
 
+function stackedBarChart(values, legend, chartWidth, chartHeight, barWidth, maxY, space, labelColour, labelALign, element) {
+
+  $(element).css({width: chartWidth + "px", height: chartHeight + "px"});
+
+  var barHeight;
+
+  for (var i = 0; i < values.length; i++) {
+    var stackHeight = 0;
+
+    for (var j = 0; j < values[i].length; j++) {
+      var bar = $("<div>").addClass("bar");
+
+      barHeight = values[i][j] / maxY * chartHeight;
+
+      $(bar).css({width: barWidth + "px", height: barHeight + "px", marginLeft: (space + i * (barWidth + space)) + "px", top: (chartHeight - barHeight - stackHeight + 100) + "px", background: legend[j][1], color: labelColour});
+      $(bar).text(values[i][j]);
+
+      if (barHeight < 16) {
+        $(bar).text("");
+      } else if (labelALign === "center") {
+        $(bar).css({lineHeight: barHeight + "px"});
+      } else if (labelALign === "bottom") {
+        $(bar).css({lineHeight: (2 * barHeight - 20) + "px"});
+      } else {
+        $(bar).css({lineHeight: 20 + "px"});
+      }
+
+      stackHeight += barHeight;
+
+      $(element).append(bar);
+    }
+  }
+}
+
 function drawXlabels(labels, chartWidth, chartHeight, barWidth, space, element) {
 
-  var labelArea = $("<div>").attr("id","div3");
+  var labelArea = $("<div>").attr("id","xArea");
 
   $(labelArea).css({width: chartWidth + "px", marginTop: (chartHeight + 1) + "px"});
 
   $(element).append(labelArea);
 
   for (var i = 0; i < labels.length; i++) {
-    var label = $("<div>").addClass("div4");
+    var label = $("<div>").addClass("xLabel");
 
     $(label).css({height: barWidth + "px", marginLeft: (space + i * (barWidth + space)) + "px", lineHeight: barWidth + "px"});
     $(label).text(labels[i]);
@@ -148,16 +182,16 @@ function drawXlabels(labels, chartWidth, chartHeight, barWidth, space, element) 
 
 function drawYlabels(scale, maxY, chartHeight, element) {
 
-  var labelArea = $("<div>").attr("id","div5");
+  var labelArea = $("<div>").attr("id","yArea");
 
-  $(labelArea).css({height: chartHeight});
+  $(labelArea).css({height: chartHeight + "px"});
 
   $(element).append(labelArea);
 
   var labelHeight;
 
   for (var i = 0; i <= maxY / scale; i++) {
-    var label = $("<div>").addClass("div6");
+    var label = $("<div>").addClass("yLabel");
 
     labelHeight = ((i * scale) / maxY) * chartHeight;
 
@@ -170,7 +204,7 @@ function drawYlabels(scale, maxY, chartHeight, element) {
 
 function drawTitle(text, size, colour, chartWidth, element) {
 
-  var title = $("<div>").attr("id","div7");
+  var title = $("<div>").attr("id","titleArea");
 
   $(title).css({width: chartWidth + "px", color: colour, fontSize: size + "pt"});
   $(title).text(text);
@@ -178,4 +212,26 @@ function drawTitle(text, size, colour, chartWidth, element) {
   $(element).append(title);
 }
 
-drawBarChart({values: [9.5,6,6,5,2.5,8,4], labels: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"], scale: 2, title: "Hours Spent Coding, Week of Sept 16 - 22"}, {width: 600, height: 400, spacing: 20, colour: "#008080", labelColour: "#00FFFF", labelAlign: "center", titleColour: "#000080", titleSize: 18}, $("#div1")[0]);
+function drawLegend(chartWidth, chartHeight, legend, element) {
+
+  var legendArea = $("<div>").attr("id","legendArea");
+
+  $(legendArea).css({height: chartHeight + "px", marginLeft: (chartWidth + 25) + "px"});
+  $(legendArea).text("Legend:")
+
+  $(element).append(legendArea);
+
+  for (var i = 0; i < legend.length; i++) {
+    var colourBox = $("<div>").addClass("legendLabel");
+    var textBox = $("<div>").addClass("legendLabel");
+
+    $(colourBox).css({background: legend[i][1], marginTop: (40 * i + 10) + "px"});
+    $(textBox).css({width: 170 + "px", marginTop: (40 * i + 10) + "px", marginLeft: 30 + "px"});
+    $(textBox).text(legend[i][0]);
+
+    $(legendArea).append(colourBox,textBox);
+  }
+}
+
+//drawBarChart({values: [9.5,6,6,5,2.5,8,4], labels: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"], scale: 2, title: "Hours Spent Coding, Week of Sept 16 - 22"}, {width: 600, height: 400, spacing: 20, colour: "#008080", labelColour: "#00FFFF", labelAlign: "center", titleColour: "#000080", titleSize: 18}, $("#chartArea")[0]);
+//drawBarChart({values: [[1000000,2000000,3000000,4000000],[2000000,4000000,6000000,8000000],[0,5000000,10000000,20000000],[4000000,3000000,2000000,1000000]], labels: ["Label1","Label2","Label3","Label4"], legend: [["Legend1","#008080"],["Legend2","#00FFFF"],["Legend3","#0000FF"],["Legend4","#000080"]], scale: 2000000, title: "Stacked Bar Chart"}, {height:500, width:500, spacing:40, labelColour:"#C0C0C0", labelAlign:"center", titleColour:"#000000", titleSize:16}, $("#chartArea")[0]);
